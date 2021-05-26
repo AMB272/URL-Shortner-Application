6555757442cb9759ac6cbd1508f4161d514137fe
Aman Bandekar <amanmb27@gmail.com>
2021-05-27 02:43:12 +0530

    added few more input fields

--- Scripts/OMMN/urlshortener/models.py
+++ Scripts/OMMN/urlshortener/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class OMMNUrl(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
     return str(self.url)
