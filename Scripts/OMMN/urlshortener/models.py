ed3b97d116e200648bdfeff3a70d06baf33c3ff6
Aman Bandekar <amanmb27@gmail.com>
2021-05-28 00:31:33 +0530

    added a logic to generate shorcodes using letters and digits

--- Scripts/OMMN/urlshortener/models.py
+++ Scripts/OMMN/urlshortener/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import random, string
+
+def Code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 # Create your models here.
 class OMMNUrl(models.Model):
@@ -7,5 +11,10 @@ class OMMNUrl(models.Model):
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    return str(self.url)
+    def save(self, *args, **kwargs):
+        #print("something")
+        self.shortcode = Code_generator()
+        super(OMMNUrl, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.url)
