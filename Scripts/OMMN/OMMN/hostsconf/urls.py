d4af5aac9829e03acac6851b5d8edf5e8469b524
Aman Bandekar <amanmb27@gmail.com>
2021-06-04 03:39:31 +0530

    bug fix commit

--- Scripts/OMMN/OMMN/hostsconf/urls.py
+++ Scripts/OMMN/OMMN/hostsconf/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from .views import wildcard_redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', wildcard_redirect),
     path('<slug:shortcode>/', wildcard_redirect),
 ]
